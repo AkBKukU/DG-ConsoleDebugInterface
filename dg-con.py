8059cb688f35e09c9fab951a6f231ae32deb718f
Shelby Jueden <shelby@jueden.org>
2025-03-09 11:12:41 -0700

    Can dump data to binary file

--- dg-con.py
+++ dg-con.py
@@ -1,34 +1,57 @@
- #!/usr/bin/env python3
+#!/usr/bin/env python3
+import serial
+import json
+import re
+import struct
 
- class SerialWrapper(object)
+class SerialWrapper(object):
 
-         def __init__(self):
-                 print("This would be a serial device interface")
+        def __init__(self,serial_port="/dev/ttyUSB0"):
+                print("This would be a serial device interface")
+                self.ser=serial.Serial(serial_port,9600,timeout=0.1)
 
-         def write(self,data):
-                 print(f"Write: {{data}}")
+        def write(self,data):
+                print(f"Write: {data}")
+                # Send data to DECTalk
+                self.ser.write( bytes(str(data),'ascii',errors='ignore') )
+                return
 
-         def read(self):
-                 print(f"Write: {{data}}")
+        def read(self):
+                result = ""
+                return str(self.ser.read(100).decode('ascii'))
 
 
- class DGnovaMicro(SerialWrapper):
 
-         # The microNOVA Debug Console remembers whater the last value entered
-         # or displayed is and will perform the next action using that value.
-         #
+class DGnovaMicro(SerialWrapper):
 
-         def __init__(self):
-                 self.machine_name="microNOVA"
-                 self.sample_read={"command":"000001/\n","response":"000001 123456 "}
-                 self.cmd_next="\n"
-                 self.cmd_read="/"
-                 self.cmd_cancel="k"
+        # The microNOVA Debug Console remembers whater the last value entered
+        # or displayed is and will perform the next action using that value.
+        #
+
+        def __init__(self,serial_port="/dev/ttyUSB0"):
+                super().__init__(serial_port)
+                self.machine_name="microNOVA"
+                self.sample_read={"command":"000001/\n","response":"000001 123456 "}
+                self.cmd_next="\n"
+                self.cmd_read="/"
+                self.cmd_cancel="k"
+                self.response_read_address_regex=self.cmd_read+"[0-1][0-7]{5} [0-1][0-7]{5}"
+
+                # Init serial interface
+                self.write(self.cmd_cancel)
+                self.read()
 
         def address_read(self,address):
+                address=str(oct(address)).replace("0o","")
                 self.write(address)
                 self.write(self.cmd_read)
-                return self.read().replace(address+" ","").strip()
+                result = self.read()
+                if not re.search(address+self.response_read_address_regex,result):
+                        raise Exception(f"Result: {result} did not match {address+self.response_read_address_regex}")
+
+                result=result.replace(address+self.cmd_read+address.zfill(6)+" ","")
+                print({address:result})
+                return int(result,8)
 
         def address_read_range(self,start,end=0,count=0):
                 responses=[]
@@ -36,14 +59,30 @@
                         end=start+count
 
                 # Set starting address
-                self.write(start)
-                self.write(self.cmd_read)
 
                 for address in range(start,end):
-                        responses.append(self.read().replace(address+" ","").strip())
-                        if address == end:
-                                break
-
-                        self.write(self.cmd_next)
+                        result=self.address_read(address)
+                        responses.append(result)
 
                 return responses
+
+dg = DGnovaMicro(serial_port="/dev/ttyUSB1")
+out_file = open("ROM-bottom.json", "w")
+
+
+data = dg.address_read_range(0o77400,end=0o77557)
+
+with open("ROM-bottom.bin", "wb") as w:
+        for value in data:
+                w.write(struct.pack('<H', value))
+
+
+json.dump(data, out_file, indent = 6)
+
+out_file = open("ROM-top.json", "w")
+data = dg.address_read_range(0o77600,end=0o77777)
+
+with open("ROM-top.bin", "wb") as w:
+        for value in data:
+                w.write(struct.pack('<H', value))
+json.dump(data, out_file, indent = 6)
