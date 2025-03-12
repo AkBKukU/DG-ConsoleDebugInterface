e6da0d29b1bcb5ff046da3033b2d49c6ec0b4f48
Shelby Jueden <shelby@jueden.org>
2025-03-12 08:05:50 -0700

    Commend and cleanup

--- dg-con.py
+++ dg-con.py
@@ -8,18 +8,20 @@ import os
 class SerialWrapper(object):
 
         def __init__(self,serial_port="/dev/ttyUSB0"):
-                print("This would be a serial device interface")
+                # Use standard 9600,8N1 config
+                # NOTE: timeout is needed for reads that are set to be too long
                 self.ser=serial.Serial(serial_port,9600,timeout=0.1)
 
         def write(self,data):
                 print(f"Write: {data}")
-                # Send data to DECTalk
                 self.ser.write( bytes(str(data),'ascii',errors='ignore') )
                 return
 
         def read(self):
                 result = ""
-                return str(self.ser.read(100).decode('ascii'))
+                result = str(self.ser.read(100).decode('ascii'))
+                print(f"Read: {result}")
+                return result
 
 
 
@@ -43,51 +45,67 @@ class DGnovaMicro(SerialWrapper):
                 self.read()
 
         def address_read(self,address):
+                # Input address in Octal format and convert to string
                 address=str(oct(address)).replace("0o","")
-                self.write(address)
-                self.write(self.cmd_read)
+                # Open address in console
+                self.write(address+self.cmd_read)
+                # Read back data
                 result = self.read()
+                # Check that data matches expected format
                 if not re.search(address+self.response_read_address_regex,result):
                         raise Exception(f"Result: {result} did not match {address+self.response_read_address_regex}")
 
+                # Filter out only address value
                 result=result.replace(address+self.cmd_read+address.zfill(6)+" ","")
                 print({address:result})
                 return int(result,8)
 
         def address_write(self,address, data):
-                data_num=data
+                # Open address location
                 self.address_read(address)
+                # Format value from octal to string
                 data=str(oct(data)).replace("0o","")
-                self.write(data)
-                self.write(self.cmd_next)
+                # Set address value
+                self.write(data+self.cmd_next)
+                # Clear read buffer of echo
                 result = self.read()
                 return
 
 
         def address_write_file(self,address,filename):
-
+                # Open address location
                 self.address_read(address)
+                # Get size of file in 16b words
                 count = os.path.getsize(filename)/2
 
+                # Open file and loop over words
                 with open(filename, "rb") as f:
                         while count:
+                                # Get data from file as octal value
                                 data=str(oct(struct.unpack('<H', f.read(2))[0])).replace("0o","")
-                                self.write(data)
-                                self.write(self.cmd_next)
+                                # Write data to address and move to next address
+                                self.write(data+self.cmd_next)
+                                # Clear read buffer of echo
                                 result = self.read()
-                                count-=2
+                                # Count down words to send
+                                count-=1
 
                 return
 
         def address_read_range(self,start,end=0,count=0):
                 responses=[]
+                # Prep end value
                 if end==0:
+                        # Count
                         end=start+count
                 else:
+                        # End address asumed to be range and will match end
+                        # prematurely
                         end+=1
 
-                # Set starting address
-
+                # Read each address
+                # TODO: Open starting address and cmd_next through others
+                # to improve speed of reads
                 for address in range(start,end):
                         result=self.address_read(address)
                         responses.append(result)
@@ -95,12 +113,13 @@ class DGnovaMicro(SerialWrapper):
                 return responses
 
         def address_read_range_file(self,start,filename,end=0,count=0):
-
+                # Read address range
                 if end==0:
-                        end=start+count
+                        data = self.address_read_range(start,count=count)
+                else:
+                        data = self.address_read_range(start,end=end)
 
-                data = self.address_read_range(start,end=end)
-
+                # Write out binary file with data
                 with open(filename, "wb") as w:
                         for value in data:
                                 w.write(struct.pack('<H', value))
